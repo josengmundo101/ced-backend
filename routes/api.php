3aa6bac6ab4a7ae2ff454e1bcdca758dcfee93a1
Joseph1901 <your_email@example.com>
2025-11-07 08:47:09 +0800

    Fix: api routes

--- routes/api.php
+++ routes/api.php
@@ -6,35 +6,48 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProjectController;
 
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application.
+| These routes are loaded by the RouteServiceProvider within a group
+| which is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
 
+// Public Routes
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+// Protected Routes (Requires Auth via Sanctum)
+Route::middleware('auth:sanctum')->group(function () {
+    
+    // Auth
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/me', [AuthController::class, 'me'])->name('me');
+
+    // Users
+    Route::prefix('users')->group(function () {
+        Route::get('/', [UserController::class, 'index'])->name('users.index');
+        Route::get('/{id}', [UserController::class, 'show'])->name('users.show');
+        Route::post('/', [UserController::class, 'store'])->name('users.store');
+        Route::put('/{id}', [UserController::class, 'update'])->name('users.update');
+        Route::delete('/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+        Route::post('/change-password', [UserController::class, 'changePassword'])->name('users.change-password');
+    });
+
+    // Projects
+    Route::prefix('projects')->group(function () {
+        Route::get('/', [ProjectController::class, 'index'])->name('projects.index');
+        Route::post('/', [ProjectController::class, 'store'])->name('projects.store');
+        Route::get('/{id}', [ProjectController::class, 'show'])->name('projects.show');
+        Route::put('/{id}', [ProjectController::class, 'update'])->name('projects.update');
+        Route::delete('/{id}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+    });
+});
+
+// Authenticated user test route (optional, can be removed)
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-// Public
-Route::post('/login', [AuthController::class, 'login']);
-
-
-// Protected
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/me', [AuthController::class, 'me']);
-
-    // Users
-    Route::get('/users', [UserController::class, 'index']);
-    Route::get('/users/{id}', [UserController::class, 'show']);
-    Route::post('/users', [UserController::class, 'store']);
-    Route::put('/users/{id}', [UserController::class, 'update']);
-    Route::delete('/users/{id}', [UserController::class, 'destroy']);
-    Route::post('/users/change-password', [UserController::class, 'changePassword']);
-
-    
-
-    // Projects
-    Route::get('/projects', [ProjectController::class, 'index']);
-    Route::post('/projects', [ProjectController::class, 'store']);
-    Route::get('/projects/{id}', [ProjectController::class, 'show']);
-    Route::put('/projects/{id}', [ProjectController::class, 'update']);
-    Route::delete('/projects/{id}', [ProjectController::class, 'destroy']);
-
-});
